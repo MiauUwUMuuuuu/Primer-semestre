cdf9f662c9e14c67e26d9f8be21763430ead32ea
MiauUwUMuuuuu <diego.donoso.montero03@gmail.com>
2024-07-18 18:21:00 -0400

    Prueba 3
    u.u

---
+++ P3-24/main.py
@@ -0,0 +1,12 @@
+# ------ NO BORRAR -----
+from funciones import *
+from herramientas import *
+# ------ NO BORRAR -----
+
+user = {}
+menu = []
+
+#menu principal
+while (True):
+    ...
+    #escribir menu con funciones aqui
